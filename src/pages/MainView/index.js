b2d139ce2f74a86e46fbd4ca0b7399354ec1cb61
nikushaomanidze <omanidze.nika@gmail.com>
2026-02-06 17:55:51 +0400

    mplement screen when there are no results after item searching

--- src/pages/MainView/index.js
+++ src/pages/MainView/index.js
@@ -111,6 +111,7 @@ export const MainView = () => {
           ? html` <${EmptyCollectionView}
               selectedFolder=${selectedFolder}
               isFavoritesView=${isFavoritesView}
+              isSearchActive=${Boolean(searchValue)}
             />`
           : html` <${ContentWrapper}>
               <${RecordListView}
